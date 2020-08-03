11ab73bd068f1ee740e4071a01e2b7e762e52e9b
junukim <junukim.dev@gmail.com>
2020-08-03 11:36:50 +0900

    deploy

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 	"github.com/makeToon/maketoon_server/database"
 	"github.com/makeToon/maketoon_server/handler"
 	"github.com/makeToon/maketoon_server/route"
+	"os"
 )
 
 func main() {
 	if err := env.Parse(&handler.Envs); err != nil {
 		fmt.Printf("%+v\n", err)
 	}
+	port := os.Getenv("PORT")
 
 	handler.AwsConfig()
 	database.MongoConn()
 	router := route.Init()
-	router.Logger.Fatal(router.Start(":8080"))
+	router.Logger.Fatal(router.Start(port))
 	//router.Logger.Fatal(router.Start(handler.Envs.Port))
 }
